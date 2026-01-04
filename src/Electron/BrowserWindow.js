f29a50d741b02683696e2721d51092c8fcacb4e8
Void <void@sinux.xyz>
2026-01-04 22:10:36 +0100

    Add files via upload
    Fixing of PIP button, Code clean up and removal of smoothscroll (too resource intensive)

--- src/Electron/BrowserWindow.js
+++ src/Electron/BrowserWindow.js
@@ -1,22 +1,21 @@
-const Electron = require('electron');
+const { BrowserWindow: ElectronBrowserWindow } = require('electron');
 const scripts = require('../util/scripts');
 const path = require('path');
 const crypto = require('crypto');
 
 function normalizeTime(time) {
-  if (time === undefined || time === null || isNaN(time)) return 0;
-  return time > 100000 ? time / 1000 : time;
+  if (!time || isNaN(time)) return 0;
+  return time > 100_000 ? time / 1000 : time;
 }
 
 function md5(string) {
   return crypto.createHash('md5').update(string).digest('hex');
 }
 
-module.exports = class BrowserWindow extends Electron.BrowserWindow {
+module.exports = class BrowserWindow extends ElectronBrowserWindow {
   constructor({ title, icon, rpc }) {
     super({
       backgroundColor: '#141414',
-      useContentSize: false,
       autoHideMenuBar: true,
       resizable: true,
       center: true,
@@ -37,77 +36,76 @@ module.exports = class BrowserWindow extends Electron.BrowserWindow {
     this.browsingStart = null;
   }
 
+  // Wrapper for webContents.executeJavaScript
   eval(code) {
     return this.webContents.executeJavaScript(code);
   }
 
+  // Get Netflix page info via injected script
   getInfos() {
     return this.eval(`(${scripts.infos})()`);
   }
 
-async checkNetflix() {
-  try {
-    const infos = await this.getInfos();
-    if (!infos) return;
+  // Main Netflix check
+  async checkNetflix() {
+    try {
+      const infos = await this.getInfos();
+      if (!infos) return;
 
-    console.log('Buttons payload:', infos.buttons);
+      const now = Date.now();
+      let elapsedMs = 0;
+      let startTimestamp;
+      let endTimestamp;
 
-    const now = Date.now();
-    let elapsedMs = 0;
-    let startTimestamp;
-    let endTimestamp;
+      const isBrowsing = infos.name === 'Browsing';
+      const isPaused = infos.paused;
 
-    const isBrowsing = infos.name === 'Browsing';
-    const isPaused = infos.paused;
+      // Browsing logic
+      if (isBrowsing) {
+        this.browsingStart ||= now; // initialize if null
+        elapsedMs = now - this.browsingStart;
 
-    if (isBrowsing) {
-      if (!this.browsingStart) this.browsingStart = now;
-      elapsedMs = now - this.browsingStart;
+        if (!isPaused) startTimestamp = new Date(this.browsingStart);
+      } 
+      // Watching a video
+      else if (infos.duration && infos.currentTime) {
+        this.browsingStart = null;
 
-      if (!isPaused) {
-        startTimestamp = new Date(this.browsingStart);
+        const currentTimeSec = normalizeTime(infos.currentTime);
+        const durationSec = normalizeTime(infos.duration);
+
+        elapsedMs = currentTimeSec * 1000;
+
+        if (!isPaused) {
+          startTimestamp = new Date(now - elapsedMs);
+          endTimestamp = new Date(startTimestamp.getTime() + durationSec * 1000);
+        }
+      } 
+      // Neither browsing nor watching
+      else {
+        this.browsingStart = null;
       }
-    } else if (infos.duration && infos.currentTime) {
-      this.browsingStart = null;
 
-      const currentTimeSec = normalizeTime(infos.currentTime);
-      const durationSec = normalizeTime(infos.duration);
+      // Send activity to Discord RPC
+      await this.rpc.setWatchingActivity({
+        title: infos.title || 'Watching Netflix',
+        state: infos.state || '',
+        avatar: infos.avatar ? md5(infos.avatar) : '',
+        userName: infos.userName || '',
+        paused: !!isPaused,
+        elapsedMs,
+        durationMs: isBrowsing ? undefined : (infos.duration ? normalizeTime(infos.duration) * 1000 : undefined),
+        ...(isPaused ? {} : { startTimestamp, endTimestamp }),
+        buttons: infos.buttons?.length ? infos.buttons : undefined,
+      });
 
-      elapsedMs = currentTimeSec * 1000;
+      // Debug logs
+      console.log('[checkNetflix] title:', infos.title);
+      console.log('[checkNetflix] state:', infos.state);
+      console.log('[checkNetflix] elapsedMs:', elapsedMs, 'startTimestamp:', startTimestamp, 'endTimestamp:', endTimestamp);
 
-      if (!isPaused) {
-        startTimestamp = new Date(now - elapsedMs);
-        endTimestamp = new Date(startTimestamp.getTime() + durationSec * 1000);
-      }
-    } else {
-      this.browsingStart = null;
+    } catch (err) {
+      console.error('[checkNetflix] error:', err);
     }
-
-    console.log('[RPC] Sending elapsedMs:', elapsedMs);
-    if (startTimestamp) console.log('[RPC] startTimestamp:', startTimestamp);
-    if (endTimestamp) console.log('[RPC] endTimestamp:', endTimestamp);
-
-    await this.rpc.setWatchingActivity({
-      title: infos.title || 'Watching Netflix',
-      state: infos.state || '',
-      avatar: infos.avatar ? md5(infos.avatar) : '',
-      userName: infos.userName || '',
-      paused: isPaused || false,
-       elapsedMs,
-  durationMs: isBrowsing ? undefined : (infos.duration ? normalizeTime(infos.duration) * 1000 : undefined),
-      // Only include timestamps if not paused
-      ...(isPaused ? {} : {
-        startTimestamp,
-        endTimestamp,
-      }),
-      buttons: infos.buttons?.length ? infos.buttons : undefined,
-    });
-
-    console.log('[checkNetflix] Sending title:', infos.title);
-    console.log('[checkNetflix] Sending state:', infos.state);
-
-  } catch (err) {
-    console.error('[checkNetflix] error:', err);
   }
-}
-}
+};
